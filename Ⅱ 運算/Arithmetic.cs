9b4defb52830bc6cb93cbf348d85268312e11f98
sushi3085 <s0943020@gm.ncue.edu.tw>
2021-07-14 19:51:47 +0800

    add Arithmetic.cs

---
+++ Ⅱ 運算/Arithmetic.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Tutorial
+{
+	class Calculator
+	{
+		public static void Main(string[] args)
+		{
+			5 + 2; 0.1 + 0.2;
+			5 - 5;
+			5 * 0; 5 * 5;
+			5 / 2;
+			// 5 / 0
+			
+			5 ** 1; 5;
+			5 ** 2; 5 * 5;
+			5 ** 3; 5 * 5 * 5;
+			
+			7 % 2;
+			6 % 2;
+			5 % 2;
+			4 % 2;
+			3 % 2;
+			1 % 2;
+		}
+	}
+}
