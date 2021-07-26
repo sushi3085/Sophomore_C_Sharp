7752f4d591830f3937dc610bc56e01e959f26ffe
sushi3085 <s0943020@gm.ncue.edu.tw>
2021-07-26 21:07:40 +0800

    update from local 01

---
+++ Ⅲ 基本資料型別(Data type)/NumberTypes.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Tutorial
+{
+	class DatatypeOfNumber
+	{
+		public static void Main(string[] args)
+		{
+			// 整數：          // initial value
+			int integerA;     // 0
+			short shortA;     // 0
+			long longA;       // 0l
+			// 可帶有小數：
+			float floatA;     // 0.0f
+			double doubleA;   // 0.0d
+			decimal decimalA; // 0.0m  // 不太常用到
+			
+			/*== == == == == == == ==*/
+			// 不常用到的datatype
+			sbyte sbyteA;     // 0
+			byte byteA;       // 0
+			
+			ushort ushortA;   // 0
+			
+			uint uintA;       // 0
+			
+			ulong ulongA;     // 0
+		}
+	}
+}
---
+++ Ⅲ 基本資料型別(Data type)/WordTypes.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Tutorial
+{
+	class DatatypeOfWord
+	{
+		public static void Main(string[] args)
+		{
+			// 字元：          // initial value
+			char charA;       // 0 (U+0000)
+			
+			/*== == == == == == == == == ==*/
+			
+			// 字串：          // initial value
+			string stringA;   // ""
+		}
+	}
+}
